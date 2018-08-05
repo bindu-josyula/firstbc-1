23394323353eb410a84b3cd7df2db4acac70666d
bindu-josyula <bindu.madhavi.j@gmail.com>
2018-08-05 17:30:33 +0800

    test

---
+++ src/com/edu/bc/Block.java
@@ -0,0 +1,40 @@
+package com.edu.bc;
+
+import java.util.Date;
+
+public class Block {
+
+	public String hash; //holds the digital signature
+	public String previousHash; // hold the hash of the previous data
+	private String data; //our data will be a simple message.
+	private long timeStamp; //as number of milliseconds since 1/1/1970.
+	private int nonce;
+	
+	//Block Constructor.
+	public Block(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		this.hash = calculateHash(); //Making sure we do this after we set the other values.
+	}
+	
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				Integer.toString(nonce) + 
+				data 
+				);
+		return calculatedhash;
+	}
+	
+	public void mineBlock(int difficulty) {		
+		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		System.out.println("Hash before mining !!!!!! : " + hash);
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+	}
+}
